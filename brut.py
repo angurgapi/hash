48363d9871527981bb36bde82a198c76c50ed0f1
angurgapi <57091094+angurgapi@users.noreply.github.com>
2020-01-22 21:07:29 +0300

    Add files via upload
    as long as the range in 'guess' function is 7, you can decode a string up to 7 symbols (lowercase letters and/or numbers)
    
    to run in terminal: path to the code file on your computer + '<<<' + hash string

---
+++ brut.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import uuid
+import hashlib
+import itertools
+import sys
+
+
+alf = 'abcdefghijklmnopqrstuvwxyz1234567890'
+
+def hash_password(password):
+	return hashlib.sha256(password.encode()).hexdigest()
+
+def guess(hash):
+	for l in range(7):
+		for try_ in itertools.product(alf, repeat=l + 1):
+			if hash_password(''.join(try_).strip()) == hash:
+				print(''.join(try_))
+
+for i in sys.stdin:
+	guess(i.strip())
